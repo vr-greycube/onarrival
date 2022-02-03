1694583ff193f7ac4f6f457e33dc8c8f1e876c94
ashish-greycube <mr.ashish.shah@gmail.com>
2022-02-03 11:09:24 +0100

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Onarrival
+
+Travel connection customization
+
+#### License
+
+MIT
---
+++ onarrival/config/desktop.py
@@ -0,0 +1,12 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Onarrival",
+			"color": "red",
+			"icon": "octicon octicon-git-fork",
+			"type": "module",
+			"label": _("Onarrival")
+		}
+	]
